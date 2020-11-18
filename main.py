0154378dd6c6ecc52a775cea0ba0188be320a0b5
Richard <rrosenthal2013@gmail.com>
2020-11-18 02:12:40 +0000

    fix error

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-input("What is your name?")
+#Variable exercise
+
+name = input("What is your name?")
+length = len(name)
+print("Hello " + name)
+print("Your name has " + str(length) + " characters in it")
+print("Pretty cool huh?")
